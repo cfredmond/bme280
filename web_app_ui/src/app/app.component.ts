23e69afa286e31c630fc48edcdb88bf6433cd8b1
Charles <charles.redmond@gmail.com>
2018-08-08 09:53:51 -0400

    updates

--- web_app_ui/src/app/app.component.ts
+++ web_app_ui/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Bme280 } from './bme280';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { Bme280 } from './bme280';
 export class AppComponent implements OnInit {
 
 	bme280: Bme280;
+  chart: any;
 
   constructor(
   private http: HttpClient) {
@@ -21,6 +23,43 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.chart = new Chart('canvas', {
+        type: 'line',
+        data: {
+            labels: ["today", "tomm", "blah", "blah", "blah", "ok"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
   	setInterval(() => {
   		this.http.get('/sensor-data').
   			subscribe((bme280: Bme280) => {
