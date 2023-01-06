fe27e24aa5db6a45e79d8a08187d684055b81a95
Charles Redmond <charles.redmond@gmail.com>
2023-01-06 09:25:31 -0500

    updates to get publishing package to work

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='bme280',
-    packages=find_packages(include=['bme280']),
+    name='cfredmond_bme280',
+    packages=find_packages(include=['cfredmond_bme280']),
     version='0.1.0',
     description='BME280 enviornment sensor library',
     author='Charles Redmond',
